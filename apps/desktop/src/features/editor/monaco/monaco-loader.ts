5af20d4700d0420392943e9915e9e39baa2c3cd5
ladieman217 <12859005+ladieman217@users.noreply.github.com>
2026-08-29 23:15:15 +0800

    chore: resolve dependency security alerts

--- apps/desktop/src/features/editor/monaco/monaco-loader.ts
+++ apps/desktop/src/features/editor/monaco/monaco-loader.ts
@@ -56,11 +56,11 @@ async function setupMonaco() {
     shikiMonaco,
   ] = await Promise.all([
     import("monaco-editor"),
-    import("monaco-editor/esm/vs/editor/editor.worker?worker"),
-    import("monaco-editor/esm/vs/language/json/json.worker?worker"),
-    import("monaco-editor/esm/vs/language/css/css.worker?worker"),
-    import("monaco-editor/esm/vs/language/html/html.worker?worker"),
-    import("monaco-editor/esm/vs/language/typescript/ts.worker?worker"),
+    import("monaco-editor/editor/editor.worker.js?worker"),
+    import("monaco-editor/language/json/json.worker.js?worker"),
+    import("monaco-editor/language/css/css.worker.js?worker"),
+    import("monaco-editor/language/html/html.worker.js?worker"),
+    import("monaco-editor/language/typescript/ts.worker.js?worker"),
     import("shiki"),
     import("@shikijs/monaco"),
   ]);
